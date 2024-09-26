8de95d1c058f0acfb4a61425bf50f53dffa81b0e
1464660354 <61771723+1464660354@users.noreply.github.com>
2024-09-26 17:14:06 +0800

    Add files via upload

---
+++ src/universe/content/content/UnTechTree.java
@@ -0,0 +1,26 @@
+package universe.content;
+
+import arc.struct.*;
+import mindustry.content.TechTree;
+import mindustry.type.*;
+
+import static mindustry.content.Planets.serpulo;
+import static mindustry.content.TechTree.node;
+import static universe.content.UnBlocks.universeCore;
+
+public class UnTechTree {
+
+    public static void load() {
+        TechTree.TechNode root = TechTree.nodeRoot("universe", universeCore, () -> {
+            TechTree.node(UnBlocks.plasmaCannon, () -> {});
+            TechTree.node(UnBlocks.proliferationCannon, () -> {});
+            TechTree.node(UnBlocks.bronzerSmelter, () -> {
+                TechTree.node(UnBlocks.bronzeDrillBit, () -> {});
+                TechTree.node(UnBlocks.bronzeAncientCannon);
+                TechTree.node(UnBlocks.bronzerWall, () -> {
+                    TechTree.node(UnBlocks.bronzerWallLarge, () -> {});
+                });
+            });
+        });
+    }
+}
